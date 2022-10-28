7a17cc48c9a7ce75b3358fca1251f42100af5df9
Dania_Busarev <sir.qnon@gmail.com>
2022-10-28 12:39:24 +0300

    Commit 4, create elif and init

--- mane.py
+++ mane.py
@@ -1,4 +1,7 @@
 def mane_function(abc): # проверка числа
+    abc_1 = int(abc)
     if abc < 0:
-        print(f"ваше число abc {abc} меньше 0")
-    
+        print(f"ваше число {abc_1} меньше 0")
+    elif abc > 0:
+        print(f"ваше число {abc_1} abc")
+        
