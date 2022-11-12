1c4ed9df835de1e57d6d8d8736b626b9192ca73d
Dania_Busarev <sir.qnon@gmail.com>
2022-11-12 14:02:32 +0200

    commit 26:  10.3 created my own class, and wrote exceptions, not yet completed

--- Lesson_10_Processing pardons/func_pardnos_10.3.py
+++ Lesson_10_Processing pardons/func_pardnos_10.3.py
@@ -1,2 +1,33 @@
-#Напишіть користувацький клас виключення з функціоналом на свій вибір.
-#Викличте його за допомогую інструкції raise
+# Напишіть користувацький клас виключення з функціоналом на свій вибір.
+# Викличте його за допомогую інструкції raise
+
+
+class Storage:
+    try:
+        def __init__(self, product, quantity, brand):
+            self.product = product
+            self.quantity = quantity
+            self.brand = brand
+            if product == int or brand == int or quantity == str:
+                raise Warning("incorrect input")
+        def quantity_product(self):
+            return f" product: {self.product} quantity: {self.quantity}"
+
+        def quntity_brand(self):
+            return f" under the brand: {self.brand} we have a product {self.product} and quntity {self.quantity}"
+    except Exception as ex:
+        return ex
+
+
+
+trade_in_class = Storage("SSD", 120, "Samsung")
+trade_in_class_2 = Storage("SSD", 80, "Kingston")
+trade_in_class_3 = Storage("Notebbok", 12, "HP")
+
+print(trade_in_class_2.quantity_product())
+
+print(trade_in_class_2.quntity_brand())
+print(trade_in_class.quantity_product())
+print(trade_in_class.quntity_brand())
+print(trade_in_class_3.quantity_product())
+print(trade_in_class_3.quntity_brand())
